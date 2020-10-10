1eb27370b8c0ddaff816bf9fab69c99c49391176
yluego <vcrod79@gmail.com>
2020-10-09 19:05:51 -0700

    add if else

--- script.js
+++ script.js
@@ -13,5 +13,37 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  // todo: prompt: what is the length?
+  var passwordLength = prompt("What is the length?", "8");
+  console.log(passwordLength, typeof passwordLength);
+  //    valid values are >=8 && <=128
+
+
+  // prompt: lowercase, uppercase, numeric, and / or special characters
+  var passwordChar = prompt("What characters would you like to use?");
+  if (passwordChar == "String") {
+    console.log("is a string")
+    return "abc"
+  }
+  if (passwordChar == "Intiger") {
+    console.log("is a intiger")
+    return "123"
+  } else {
+    console.log("defaulting to a string")
+    return "efg"
+  }
+
+
+  //    valid characters are 'lowercase', 'uppercase', 'numeric', 'special characters'
+
+  // confirm: above
+
+  // generate a password
+
+
+  return 0;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
